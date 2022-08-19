7fa9ae51ca935bb2125f86ca92d149f8af3cce89
dhiimr <runtungan@gmail.com>
2022-08-19 14:16:28 +0700

    3

--- install.sh
+++ install.sh
@@ -1,5 +1,10 @@
 pkg update
+termux-setup-storage
 pkg install ffmpeg -y
 wget https://raw.githubusercontent.com/dhiimr/ydownload/main/y ~/
 mv ~/y ~/../usr/bin/
 chmod +x ~/../usr/bin/y
+wget https://github.com/dhiimr/ydownload/raw/main/youtube-dl ~/
+mv ~/youtube-dl ~/../usr/bin/
+chmod +x ~/../usr/bin/youtube-dl
+mkdir /sdcard/YouTube
